3d19169860a0a49cf4bac5f0e4acce713ea2ff0e
Nimesh khatiwada <93083809+menimesh@users.noreply.github.com>
2023-01-28 21:01:27 +0545

    Add files via upload

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php 
+$a=[1,15,20,36];
+$b=array(1,5,6,7,8);
+$associative_array=array('name'=>'Nimesh','age'=>21,'Address'=>'Arjundhara');
+print_r($a);
+echo "<br>";
+print_r($b);
+echo "<br>";
+print_r($associative_array);
+?>
